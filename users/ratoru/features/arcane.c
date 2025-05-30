d2c254f9996e100cf58bbce0b72781e76aa29acb
Raphael <raphael.t.ruban@gmail.com>
2025-05-30 01:17:11 -0700

    feat: update arcane

--- users/ratoru/features/arcane.c
+++ users/ratoru/features/arcane.c
@@ -56,8 +56,11 @@ void process_arcane(uint16_t keycode, uint8_t mods, bool is_idle) {
         case KC_ENT:
             add_oneshot_mods(MOD_BIT(KC_LSFT)); // Set one-shot mod for shift.
             break;
+        case KC_BSPC:
+            SEND_STRING(SS_LALT(SS_TAP(X_BSPC)));
+            break;
         case KC_A:
-            MAGIC_STRING(SS_TAP(X_BSPC) SS_LALT(SS_TAP(X_QUOTE)) SS_TAP(X_A), KC_A);
+            MAGIC_STRING(SS_TAP(X_BSPC) SS_LALT(SS_TAP(X_U)) SS_TAP(X_A), KC_A);
             break;
         case KC_B:
             MAGIC_STRING(/*b*/ "b", KC_B);
@@ -66,7 +69,11 @@ void process_arcane(uint16_t keycode, uint8_t mods, bool is_idle) {
             MAGIC_STRING(/*c*/ "h", KC_H);
             break;
         case KC_D:
-            MAGIC_STRING(/*b*/ "d", KC_D);
+            if (!is_caps_word_on() && (mods & MOD_MASK_SHIFT)) {
+                SEND_STRING(/*D*/ "anke");
+            } else {
+                MAGIC_STRING(/*t*/ "d", KC_D);
+            }
             break;
         case KC_E:
             MAGIC_STRING(/*e*/ "u", KC_E);
@@ -99,7 +106,7 @@ void process_arcane(uint16_t keycode, uint8_t mods, bool is_idle) {
             MAGIC_STRING(/*n*/ "n", KC_N);
             break;
         case KC_O:
-            MAGIC_STRING(SS_TAP(X_BSPC) SS_LALT(SS_TAP(X_QUOTE)) SS_TAP(X_O), KC_O);
+            MAGIC_STRING(SS_TAP(X_BSPC) SS_LALT(SS_TAP(X_U)) SS_TAP(X_O), KC_O);
             break;
         case KC_P:
             MAGIC_STRING(/*p*/ "h", KC_H);
@@ -111,12 +118,7 @@ void process_arcane(uint16_t keycode, uint8_t mods, bool is_idle) {
             MAGIC_STRING(/*r*/ "r", KC_R);
             break;
         case KC_S:
-            if (!is_caps_word_on() && (mods & MOD_MASK_SHIFT)) {
-                // Checks for shift mod of previous key
-                SEND_STRING(/*S*/ "ch");
-            } else {
-                MAGIC_STRING(/*t*/ "s", KC_S);
-            }
+            MAGIC_STRING(/*s*/ "ch", KC_H);
             break;
         case KC_T:
             if (!is_caps_word_on() && (mods & MOD_MASK_SHIFT)) {
@@ -127,13 +129,13 @@ void process_arcane(uint16_t keycode, uint8_t mods, bool is_idle) {
             }
             break;
         case KC_U:
-            MAGIC_STRING(SS_TAP(X_BSPC) SS_LALT(SS_TAP(X_QUOTE)) SS_TAP(X_U), KC_U);
+            MAGIC_STRING(SS_TAP(X_BSPC) SS_LALT(SS_TAP(X_U)) SS_TAP(X_U), KC_U);
             break;
         case KC_V:
             MAGIC_STRING(/*v*/ "v", KC_V);
             break;
         case KC_W:
-            MAGIC_STRING(/*w*/ "w", KC_W);
+            MAGIC_STRING(/*w*/ "q", KC_W);
             break;
         case KC_X:
             SEND_STRING(SS_TAP(X_BSPC) SS_RALT(SS_TAP(X_S)));
